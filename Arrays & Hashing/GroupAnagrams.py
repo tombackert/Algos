3fd303c3856f0e5bb1674bbd22a13f61bb13954b
tombackert <backert.github@gmail.com>
2023-04-22 22:44:13 +0200

    more cleaner code

--- Arrays & Hashing/GroupAnagrams.py
+++ Arrays & Hashing/GroupAnagrams.py
@@ -1,11 +1,11 @@
-# Time Complexity: O(n * k * log(k))
+# Time Complexity: O(n* k * log(k))
 # Space Complexity: O(n)
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagrams = {} # O(n)
         for s in strs: # O(n)
-            sorted_s = ''.join(sorted(s)) # O(k * log(k)), k = len(s)
+            sorted_s = ''.join(sorted(s)) # O(k * log(k))
             if sorted_s in anagrams:
                 anagrams[sorted_s].append(s)
             else:
