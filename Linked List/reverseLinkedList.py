6dafac27a2f5c832b37d7b956b24bbdc7db57775
tombackert <backert.github@gmail.com>
2023-05-01 20:37:56 +0200

    new Linked List stuff

---
+++ Linked List/reverseLinkedList.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# Time complexity: O(n)
+# Space complexity: O(1)
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head
+        
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
